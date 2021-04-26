f9f02a40ea7fe2fe6f009b3eb2d49c42314195cd
BochisAndrei <bochis.andi@gmail.com>
2021-04-26 16:45:16 +0300

    App Menu

---
+++ app/src/main/java/com/example/gdsc_app_mobile/FragmentContact.kt
@@ -0,0 +1,21 @@
+package com.example.gdsc_app_mobile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class FragmentContact : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_contact, container, false)
+
+        return view
+    }
+
+}
--- app/src/main/java/com/example/gdsc_app_mobile/MainActivity.kt
+++ app/src/main/java/com/example/gdsc_app_mobile/MainActivity.kt
@@ -2,10 +2,88 @@ package com.example.gdsc_app_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.squareup.picasso.Picasso
+import jp.wasabeef.picasso.transformations.CropCircleTransformation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+    lateinit var drawerLayout : DrawerLayout
+
+    lateinit var navigationView : NavigationView
+    lateinit var toolbar : Toolbar
+    lateinit var actionBarToggle : ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //instantiate drawer components
+        setupDrawer()
+
+        if(savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.container_fragment,
+                    FragmentContact()
+                )
+                .commit()
+            navigationView.setCheckedItem(R.id.nav_contact)
+        }
+
     }
+
+    private fun setupDrawer() {
+        // set toolbar
+        toolbar = findViewById(R.id.nav_toolbar_toolbar)
+        setSupportActionBar(toolbar)
+        // get layout
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        // create navigationView's components
+        navigationView = findViewById(R.id.navigation)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        // add cropped image
+        val header = navigationView.getHeaderView(0)
+        val navHeaderImgView = header.findViewById<ImageView>(R.id.nav_header_img_view)
+        Picasso.get()
+            .load(R.drawable.img_dsc)
+            .transform(CropCircleTransformation())
+            .into(navHeaderImgView)
+        val navHeaderTitle = header.findViewById<TextView>(R.id.nav_header_title)
+        navHeaderTitle.textSize = 20f
+
+
+        actionBarToggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, toolbar,
+            R.string.open,
+            R.string.close
+        )
+
+        drawerLayout.addDrawerListener(actionBarToggle)
+        actionBarToggle.syncState()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_contact -> {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(
+                        R.id.container_fragment,
+                        FragmentContact()
+                    )
+                    .commit()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
