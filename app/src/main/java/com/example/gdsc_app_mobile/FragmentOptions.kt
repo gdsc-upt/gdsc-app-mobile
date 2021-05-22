25cfdb719bb6e5e452bac104626311e2b0c5400b
John <ioan.macavei2001@gmail.com>
2021-05-22 23:48:49 +0300

    Added basic language change functionality - to implement fragment retention

--- app/src/main/java/com/example/gdsc_app_mobile/FragmentOptions.kt
+++ app/src/main/java/com/example/gdsc_app_mobile/FragmentOptions.kt
@@ -1,19 +1,27 @@
 package com.example.gdsc_app_mobile
 
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.content.res.Resources
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CompoundButton
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
+import java.util.*
 
 
-class FragmentOptions : Fragment() {
+class FragmentOptions : Fragment(), AdapterView.OnItemSelectedListener{
 
     lateinit var switch_dark_mode: SwitchCompat
+    lateinit var language_spinner : Spinner
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +30,72 @@ class FragmentOptions : Fragment() {
         val view = inflater.inflate(R.layout.fragment_options, container, false)
 
         switch_dark_mode = view.findViewById(R.id.switch_dark_mode)
+        language_spinner = view.findViewById(R.id.language_spinner)
+        language_spinner.onItemSelectedListener = this;
+
+
+        activity!!.getSharedPreferences("save", 0).edit().putString("lastFragment","options")
+        activity!!.getSharedPreferences("save", 0).edit().commit()
+
+
 
         setupMode()
-
+        setupLanguage()
         return view
+    }
+
+    private fun changeLanguage(language : String?){
+        val sharedPreferences: SharedPreferences = activity!!.getSharedPreferences("save", 0)
+
+        val pref_language = sharedPreferences.getString("language","en");
+
+
+
+        if (pref_language != language) {
+            var myLocale = Locale("en")
+            when (language) {
+                "en" -> {
+                    myLocale = Locale("en")
+                }
+                "ro" -> {
+                    myLocale = Locale("ro")
+                }
+
+            }
+
+            val res: Resources = resources
+            val dm: DisplayMetrics = res.getDisplayMetrics()
+            val conf: Configuration = res.getConfiguration()
+            conf.locale = myLocale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(this.context,MainActivity::class.java)
+            startActivity(refresh)
+            val editor = sharedPreferences.edit()
+            editor.putString("language",language)
+            editor.apply()
+        }
+
+    }
+
+    private fun setupLanguage() {
+        val sharedPreferences: SharedPreferences = activity!!.getSharedPreferences("save", 0)
+
+        this.context?.let {
+            ArrayAdapter.createFromResource(it,R.array.Languages,R.layout.language_spinner_item).also { adapter ->
+                adapter.setDropDownViewResource(R.layout.language_spinner_item)
+                language_spinner.adapter = adapter
+            }
+        }
+
+        val language = sharedPreferences.getString("language","en");
+        changeLanguage(language)
+
+        when (language)
+        {
+            "en" -> {language_spinner.setSelection(0)}
+            "ro" -> {language_spinner.setSelection(1)}
+        }
+
     }
 
     private fun setupMode() {
@@ -55,5 +125,17 @@ class FragmentOptions : Fragment() {
                 editor.apply()
             }
         }
+    }
+
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (position)
+        {
+            0 -> {changeLanguage("en")}
+            1-> {changeLanguage("ro")}
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 }
