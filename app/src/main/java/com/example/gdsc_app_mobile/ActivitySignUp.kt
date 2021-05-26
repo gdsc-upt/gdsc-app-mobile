e4517c879ea9bea4951afd4543b3dd98a64f8ea7
Bogdan Baciu <bogdanbaciu00@yahoo.com>
2021-05-26 23:26:53 +0300

    updated sign_up and register

--- app/src/main/java/com/example/gdsc_app_mobile/ActivitySignUp.kt
+++ app/src/main/java/com/example/gdsc_app_mobile/ActivitySignUp.kt
@@ -1,11 +1,33 @@
 package com.example.gdsc_app_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class ActivitySignUp : AppCompatActivity() {
+
+    private lateinit var logInButton : Button
+    private lateinit var registerButton : TextView
+    private lateinit var username : EditText
+    private lateinit var password : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        logInButton = findViewById(R.id.log_in_button)
+        registerButton = findViewById(R.id.activity_sign_up_register)
+        username = findViewById(R.id.activity_sign_up_username)
+        password = findViewById(R.id.activity_sign_up_password)
+
+        registerButton.setOnClickListener{
+            val intent = Intent(this@ActivitySignUp, ActivityRegister::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
