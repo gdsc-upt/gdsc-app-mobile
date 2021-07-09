b21b63131a7c500620b4048c743bb13c5a078e03
Coman Alex <comanalex33@yahoo.com>
2021-07-09 21:01:26 +0300

    RecyclerView instead of ListView for members

--- app/src/main/java/com/example/gdsc_app_mobile/fragments/FragmentMembers.kt
+++ app/src/main/java/com/example/gdsc_app_mobile/fragments/FragmentMembers.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gdsc_app_mobile.HelperClass
 import com.example.gdsc_app_mobile.R
 import com.example.gdsc_app_mobile.Singleton
 import com.example.gdsc_app_mobile.activities.MainActivity
-import com.example.gdsc_app_mobile.adapters.MembersAdapter
+import com.example.gdsc_app_mobile.adapters.RVAdapterMembers
 import com.example.gdsc_app_mobile.interfaces.ISelectedDataMembers
+import com.example.gdsc_app_mobile.interfaces.OnItemClickListener
+import com.example.gdsc_app_mobile.models.FaqModel
 import com.example.gdsc_app_mobile.models.MemberModel
 import com.example.gdsc_app_mobile.models.TeamsModel
 import com.example.gdsc_app_mobile.services.ApiClient
@@ -20,7 +24,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 //This fragment is used to show the list of members for each team(when a fragment like this starts, a team is required)
-class FragmentMembers() : Fragment(), ISelectedDataMembers {
+class FragmentMembers() : Fragment(), ISelectedDataMembers, OnItemClickListener {
 
     lateinit var team: TeamsModel
     lateinit var allMembers: ArrayList<MemberModel>
@@ -29,6 +33,8 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
     lateinit var backButton: Button
     lateinit var teamMembers: ArrayList<MemberModel>
     lateinit var addMemberButton: ImageView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var rvMembersAdapter: RVAdapterMembers
 
     constructor(team: TeamsModel, position: Int) : this() {
         this.team = team
@@ -49,6 +55,13 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
         backButton = view.findViewById(R.id.button_members_back_to_teams)
         addMemberButton = view.findViewById(R.id.add_member)
 
+        // initialize recycler view
+        recyclerView = view.findViewById(R.id.members_recycler_view)
+        rvMembersAdapter = RVAdapterMembers(activity as MainActivity, this, position, requireActivity())
+        rvMembersAdapter.listenerFragment = this
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = rvMembersAdapter
+
         HelperClass.adminRole(addMemberButton)
 
         tvTeamTitle.text = team.name    //Setting the name of the team
@@ -57,9 +70,9 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
 
         backButtonCLicked()     //Back button click listener
 
-        getAllMembers()
+        getAllMembers()         //Get all members from backend
 
-        addMemberClicked()
+        addMemberClicked()      //Add member button functionality
 
         return view
     }
@@ -81,7 +94,7 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
                     if(members != null)
                         for(member in members)
                             allMembers.add(member)
-                    setListView(requireView())
+                    setListView()
                 }
                 else
                     Toast.makeText(requireContext(), resources.getString(R.string.something_wrong), Toast.LENGTH_SHORT).show()
@@ -94,23 +107,17 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
         })
     }
 
-    private fun setListView(view: View) {
+    private fun setListView() {
 
         teamMembers = ArrayList()
         for(member in allMembers)
             if(team.id == member.teamId)
                 teamMembers.add(member)     //Adding only the members with the same teamId as the ID of the team
 
-        val adapter = MembersAdapter(requireActivity(), teamMembers)
-        adapter.activity = requireActivity()
-        adapter.listener = this
-        adapter.positionColoring = position     //We set the position of the team in the list (for coloring)
-        val listView : ListView = view.findViewById(R.id.members_list_view)
-        listView.adapter = adapter
-
-        listView.divider = null     //No horizontal line between items
+        teamMembers?.let { rvMembersAdapter.setMember(it) }     //Set the member list for recyclerView
     }
 
+    //This is used to color the header of the page
     private fun colorPage(card: View) {
         when(position % 4) {
             0 -> card.setBackgroundColor(requireActivity().getColor(R.color.gdsc_yellow))
@@ -157,5 +164,8 @@ class FragmentMembers() : Fragment(), ISelectedDataMembers {
 
         })
     }
+
+    override fun onItemClick(faq: FaqModel?) {}
+    override fun onLongItemClick(faq: FaqModel?) {}
 
 }
